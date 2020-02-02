9526e549b02e268cca85e68b1a1ab0add66161bd
Chris Pitman <cpitman1@gmail.com>
2020-02-02 14:43:23 -0500

    Preserve testing artifacts after test script for archiving

--- test/e2e.sh
+++ test/e2e.sh
@@ -4,17 +4,20 @@ failures=0
 
 for test in $(find ./examples/ -maxdepth 1 -mindepth 1 -type f); do
   echo "Testing $test"
+  # create staging director for results
+  ARTIFACT_DIR=/tmp/test/$test
+  mkdir -p $ARTIFACT_DIR
   # get output of the template
-  templateOut=$(oc process --local -f $test -o yaml | yq -S .items[])
+  oc process --local -f $test -o yaml | yq -S .items[] > $ARTIFACT_DIR/oc-process-sorted.yaml
   # convert template to chart
-  template2helm convert --template $test --chart /tmp/charts > /dev/null
+  template2helm convert --template $test --chart $ARTIFACT_DIR/charts > /dev/null
   # find newly created chart
-  chart=$(ls -td /tmp/charts/*/ | head -1)
+  chart=$(ls -td $ARTIFACT_DIR/charts/*/ | head -1)
   echo "Resulting chart: $chart"
   # get output of chart
-  chartOut=$(helm template $chart | yq -S .)
+  helm template $chart | yq -S . > $ARTIFACT_DIR/helm-process-sorted.yaml
   # compare resources produced
-  gap=$(diff <(echo "$templateOut") <(echo "$chartOut"))
+  gap=$(diff $ARTIFACT_DIR/oc-process-sorted.yaml $ARTIFACT_DIR/helm-process-sorted.yaml)
   if [[ "${gap}x" != "x" ]]; then
     >&2 echo "Test Failed!"
     >&2 echo "${gap}"
@@ -26,9 +29,6 @@ for test in $(find ./examples/ -maxdepth 1 -mindepth 1 -type f); do
   echo
   echo
 done
-
-# Clean up test directory
-rm -r /tmp/charts
 
 if [[ $failures > 0 ]]; then
   echo "$failures Tests Failed"
