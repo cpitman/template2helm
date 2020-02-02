d763b7d1fdb1ffb2f819b8150e51ec3c28233512
Chris Pitman <cpitman1@gmail.com>
2020-02-02 17:10:00 -0500

    Update e2e.sh

--- test/e2e.sh
+++ test/e2e.sh
@@ -5,7 +5,7 @@ failures=0
 for test in $(find ./examples/ -maxdepth 1 -mindepth 1 -type f); do
   echo "Testing $test"
   # get output of the template
-  templateOut=$(oc process --local -f $test -o yaml | yq '.items[] | sort_by(.kind, .metadata.name)')
+  templateOut=$(oc process --local -f $test -o yaml | yq '.items | sort_by(.kind, .metadata.name)')
   # convert template to chart
   template2helm convert --template $test --chart /tmp/charts > /dev/null
   # find newly created chart
